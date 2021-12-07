ceb991053c85fe518b5e8a4d19350ff2f5f402e5
Vasil Todorovski <vasil.todorovski@intertec.io>
2021-12-07 17:30:15 +0100

    Removed wrapping div

--- src/components/CardHorizontal/CardHorizontal.jsx
+++ src/components/CardHorizontal/CardHorizontal.jsx
@@ -28,69 +28,67 @@ export const CardHorizontal = ({
   isLiked,
 }) => {
   return coupon ? (
-    <div className="col-lg-8 col-12" id="coupons">
-      <div className={`CardHorizontal ${className}`}>
-        <CouponImage src={coupon.image}>
-          <CouponImageBadge coupon={coupon} />
-          <LimitOverlay
-            message={getOverlayMessage({
-              status: coupon.status,
-              isBrandCopiesLimitReached: coupon.isBrandCopiesLimitReached,
-            })}
-          />
-          <Like
-            isLiked={coupon.isFavourite}
-            id={coupon.id}
-            handleClick={handleLikeClick}
-            isAuth={isAuth}
-            isTouchDevice={isTouchDevice}
-            isLiked={isLiked}
-          ></Like>
-        </CouponImage>
-        <div className="CardHorizontal__details">
-          <BrandLogo src={coupon.brandLogo} />
-          <Discount type={coupon.discountType}>
-            {coupon.discountType === "FREE"
-              ? coupon.freeDescription
-              : coupon.discountValue}
-          </Discount>
-          <Heading>
-            Mindestbestellwert:{" "}
-            {coupon &&
-            coupon.amountCondition &&
-            coupon.amountCondition !== 0 ? (
-              <b>{coupon.amountCondition.toLocaleString("de-DE")}</b>
-            ) : (
-              <b>keiner</b>
-            )}
-            <br />
-            Gültig bis:{" "}
-            <b>
-              {Intl.DateTimeFormat("de-DE", {
-                year: "numeric",
-                month: "2-digit",
-                day: "2-digit",
-              }).format(new Date(coupon.validationDate))}
-            </b>
-          </Heading>
-          <Description id={coupon.id}>
-            <div
-              style={{
-                wordWrap: "break-word",
-                wordBreak: "break-word",
-              }}
-              dangerouslySetInnerHTML={createMarkup(coupon.couponRegulations)}
-            ></div>
-          </Description>
-          <CouponButton
-            coupon={{
-              ...coupon,
-              ...(coupon.code && { code: coupon.code }),
-              ...(coupon.codeURL && { codeURL: coupon.codeURL }),
+    // <div className="col-lg-8 col-12" id="coupons">
+    <div className={`CardHorizontal ${className}`}>
+      <CouponImage src={coupon.image}>
+        <CouponImageBadge coupon={coupon} />
+        <LimitOverlay
+          message={getOverlayMessage({
+            status: coupon.status,
+            isBrandCopiesLimitReached: coupon.isBrandCopiesLimitReached,
+          })}
+        />
+        <Like
+          isLiked={coupon.isFavourite}
+          id={coupon.id}
+          handleClick={handleLikeClick}
+          isAuth={isAuth}
+          isTouchDevice={isTouchDevice}
+          isLiked={isLiked}
+        ></Like>
+      </CouponImage>
+      <div className="CardHorizontal__details">
+        <BrandLogo src={coupon.brandLogo} />
+        <Discount type={coupon.discountType}>
+          {coupon.discountType === "FREE"
+            ? coupon.freeDescription
+            : coupon.discountValue}
+        </Discount>
+        <Heading>
+          Mindestbestellwert:{" "}
+          {coupon && coupon.amountCondition && coupon.amountCondition !== 0 ? (
+            <b>{coupon.amountCondition.toLocaleString("de-DE")}</b>
+          ) : (
+            <b>keiner</b>
+          )}
+          <br />
+          Gültig bis:{" "}
+          <b>
+            {Intl.DateTimeFormat("de-DE", {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+            }).format(new Date(coupon.validationDate))}
+          </b>
+        </Heading>
+        <Description id={coupon.id}>
+          <div
+            style={{
+              wordWrap: "break-word",
+              wordBreak: "break-word",
             }}
-          />
-        </div>
+            dangerouslySetInnerHTML={createMarkup(coupon.couponRegulations)}
+          ></div>
+        </Description>
+        <CouponButton
+          coupon={{
+            ...coupon,
+            ...(coupon.code && { code: coupon.code }),
+            ...(coupon.codeURL && { codeURL: coupon.codeURL }),
+          }}
+        />
       </div>
     </div>
-  ) : null;
+  ) : // </div>
+  null;
 };
